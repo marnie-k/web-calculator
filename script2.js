79d7510c92492057fd0f3ed8e448e1dcb006fd9a
Marnie K <marnie.kelly.123@gmail.com>
2021-10-23 17:02:44 -0400

    adding index2.html and script2.js

---
+++ script2.js
@@ -0,0 +1,8 @@
+function calculate(){
+
+  var a = document.getElementById("a").value
+  var b = document.getElementById("b").value
+  var answer = parseInt(a) + parseInt(b)
+
+  document.getElementById("answer").innerHTML = answer
+}
